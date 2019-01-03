5764b1d965a764a6b586b7e3ccab8f4d4e1ff786
Tishana <tishana.trainor@gmail.com>
2019-01-03 14:47:26 -0500

    added supporting files

--- public/notes.js
+++ public/notes.js
@@ -53,6 +53,49 @@
 // router.put('/:id', noteController.update)
 // router.delete('/:id', noteController.delete)
 
+//View samples
+// const NoteView = Backbone.View.extend({ // one note
+//     model: new Note(),
+//     tagName: 'div',
+//     id: 'note-one',
+//     // noteTpl: _.template('notes-one'),
+//     initialize: () => {
+//         // this.template = _.template($('#note-one').html())
+//         console.log(this.el)
+//         console.log(this.$el)
+//         $('note-one').append(this.el)
+//     },
+//     events: {
+//         // 'click .new-note': 
+//     },
+//     render: () => {
+//         this.$el.html(this.template(this.model.toJSON()))
+//         this.input = this.$('.edit')
+//         return this
+//     }
+// })
+// console.log(noteView.el)
+
+// const NotesView = Backbone.View.extend({ //all notes
+//     model: notes,
+//     el: $('#notes-all'),
+//     initialize: () => {
+//         this.model.on('add', this.render(), this)
+//     },
+//     render: () => {
+//         var self = this
+//         this.$el.html('')
+//         _.each(this.model.toArray(), (note) => {
+//             self.$el.append(new NoteView({ model: note })).render().$el
+//         })
+//         return this
+//     }
+// })
+
+//HTML stuff
+/* <h4>date</h4>
+    <h3><input class="title-input"></h3>
+        <p><textarea name="text" class="text-input" cols="80" rows="10"></textarea></p> --></h3> */
 
 
 //sample model, collection
--- public/script.js
+++ public/script.js
@@ -3,69 +3,43 @@
 var Note = Backbone.Model.extend({
     defaults: {
         date: Date.now,
-        title: '',
-        text: ''
+        title: " ",
+        text: " "
     },
     initialize: () => {
-        console.log('Note model initialized...')
+        console.log("Note model initialized...")
     }
 })
-
+// var m = new Note({
+//     title: "First Note",
+//     text: "lorem Ipsum, blah blah blah..."
+// })
 
 
 //Collection
 const NotesCollection = Backbone.Collection.extend({
     model: Note,
-    // localStorage: new StorageEvent('notes')
 })
 
-var notes = new NotesCollection()
-
-
+// var notes = new NotesCollection()
 
 //View
-const NoteView = Backbone.View.extend({ // one note
-    model: new Note(),
-    tagName: 'div',
-    // noteTpl: _.template('notes-one'),
+var NoteView = Backbone.View.extend({
+    //<div class="note-one" id="unique"></div>
+    tagname: "div",
+    className: "note-one",
+    id: "unique",
     initialize: () => {
-        this.template = _.template($('#note-one').html())
-    },
-    events: {
-        // 'click .new-note': 
-    },
-    render: () => {
-        this.$el.html(this.template(this.model.toJSON()))
-        this.input = this.$('.edit')
-        return this
+        console.log(this.tagname)
+        // console.log($el)
+        // $(".note-one").append(this.el)
     }
 })
-var oneNote = new NoteView()
-// console.log(noteView.el)
 
-const NotesView = Backbone.View.extend({ //all notes
-    model: notes,
-    el: $('#notes-all'),
-    initialize: () => {
-        this.model.on('add', this.render(), this)
-    },
-    render: () => {
-        var self = this
-        this.$el.html('')
-        _.each(this.model.toArray(), (note) => {
-            self.$el.append(new NoteView({ model: note })).render().$el
-        })
-        return this
-    }
-})
-var allNotes = new NotesView()
+
+
+
 
 $(document).ready(() => {
-    $('add-note').on('click', () => {
-        var note = new Note({
-            date: Date.now,
-            title: $('.title-input').val(),
-            text: $('.text-input').val()
-        })
-    })
+    var oneView = new NoteView()
 })
